a180f2524dbb085c1badc9ce2e5d12e35ed4e9e2
fozerc <an07ka@gmail.com>
2024-09-11 18:25:20 +0200

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import './App.css'
+
+const content = [
+    {
+        tab: "Раздел 1",
+        content: "Контент для раздела 1"
+    },
+    {
+        tab: "Раздел 2",
+        content: "Контент для раздела 2"
+    },
+]
 
 const useInput = (initialValue, validator) => {
     const [value, setValue] = useState(initialValue)
@@ -16,7 +27,31 @@ const useInput = (initialValue, validator) => {
     return {value, onChange}
 };
 
+const useTabs = (initialTab, allTabs) => {
+    if (!allTabs || !Array.isArray(allTabs)) {
+        return;
+    }
+    const [currentIndex, setCurrentIndex] = useState(initialTab)
+    return {
+        currentItem: allTabs[currentIndex],
+        changeItem: setCurrentIndex
+    };
+};
+
+const useTitle = (initialTitle) => {
+    const [title, setTitle] = useState(initialTitle)
+    const updateTitle = () => {
+        const htmlTitle = document.querySelector("title")
+        htmlTitle.innerText = title
+    }
+    useEffect(updateTitle, [title])
+    return setTitle
+}
+
 function App() {
+    const titleUpdater = useTitle('Загрузка...')
+    setInterval(() => titleUpdater('Загружено'), 5000)
+    const {currentItem, changeItem} = useTabs(0, content)
     const maxLength = (value) => value.length < 10
     const checkAt = (value) => !value.includes("@", 1)
     const name = useInput('Mr.', maxLength)
@@ -24,6 +59,12 @@ function App() {
     return (
         <>
             <div>Practice</div>
+            <div className={'tabs_menu'}>
+                {content.map((section, index) => (
+                    <button onClick={() => changeItem(index)}>{section.tab}</button>
+                ))}
+                <div>{currentItem.content}</div>
+            </div>
             <input placeholder="Name" {...name}/>
             <input placeholder="Name" {...email}/>
         </>
