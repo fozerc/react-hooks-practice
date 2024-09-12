c7f286623f269e5d19fd6e22ede0a59b4a1dcb2d
fozerc <an07ka@gmail.com>
2024-09-12 21:43:28 +0200

    useRef, useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react'
+import {useEffect, useRef, useState} from 'react'
 import './App.css'
 
 const content = [
@@ -48,6 +48,24 @@ const useTitle = (initialTitle) => {
     return setTitle
 }
 
+const useClick = (onClick) => {
+    if (typeof onClick !== 'function') {
+        return;
+    }
+    const element = useRef()
+    useEffect(() => {
+        if (element.current) {
+            element.current.addEventListener('click', onClick)
+        }
+        return () => {
+            if (element.current) {
+                element.current.removeEventListener('click', onClick)
+            }
+        }
+    }, [])
+    return element;
+}
+
 function App() {
     const titleUpdater = useTitle('Загрузка...')
     setInterval(() => titleUpdater('Загружено'), 5000)
@@ -56,17 +74,22 @@ function App() {
     const checkAt = (value) => !value.includes("@", 1)
     const name = useInput('Mr.', maxLength)
     const email = useInput('email', checkAt)
+    const sayHello = () => console.log("Hello!")
+    const button = useClick(sayHello)
     return (
         <>
             <div>Practice</div>
             <div className={'tabs_menu'}>
                 {content.map((section, index) => (
-                    <button onClick={() => changeItem(index)}>{section.tab}</button>
+                    <button key={index} onClick={() => changeItem(index)}>{section.tab}</button>
                 ))}
                 <div>{currentItem.content}</div>
             </div>
             <input placeholder="Name" {...name}/>
             <input placeholder="Name" {...email}/>
+            <div className={"input_container"}>
+                <button ref={button}>Click!</button>
+            </div>
         </>
     )
 }
